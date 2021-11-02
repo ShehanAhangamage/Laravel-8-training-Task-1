89cc3094fd22b9283c4c43afc7454dca718b2c90
Shehan Ahangamage <asahangamageshehan@gmail.com>
2021-11-02 13:49:57 +0530

    decreased login component width

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'Login')
 
 @section('content')<div class="py-12">
-    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+    <div class="max-w-xl mx-auto sm:px-6 lg:px-8">
         <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-5">
             <div class="flex">
                 <div class="flex-auto text-2xl mb-4">Login</div>
