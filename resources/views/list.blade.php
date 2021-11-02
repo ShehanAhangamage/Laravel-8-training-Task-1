71df70888f18a2e8d65cf3fac48ea33561f603c9
Shehan Ahangamage <asahangamageshehan@gmail.com>
2021-11-02 10:00:28 +0530

    changed width of list view

--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
 <div class="py-12">
-    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+    <div class="max-w-8xl mx-auto sm:px-6 lg:px-8">
         <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-5">
             <div class="flex">
                 <div class="flex-auto text-2xl mb-4">Booking List</div>
